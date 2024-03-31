731b2ae05d9b2b1bcc005057b940a06461a2ca53
mike <test@mail.com>
2024-03-31 07:47:53 +0300

    Add errors to tests

--- api/src/test/kotlin/ApiTests.kt
+++ api/src/test/kotlin/ApiTests.kt
@@ -1,14 +1,22 @@
 import io.qameta.allure.Allure
+import org.testng.Assert
 import org.testng.annotations.Test
 import java.lang.Thread.sleep
 
 class ApiTests {
 
+    private var counter = 1
+
     @Test
     fun firstApiTest() {
         Allure.step {
-            sleep(5_000L)
-            println("#1 API Test")
+            sleep(1_000L)
+            if (counter == 0) {
+                println("#1 API Test")
+            } else {
+                --counter
+                Assert.fail()
+            }
         }
 
     }
@@ -16,8 +24,14 @@ class ApiTests {
     @Test
     fun secondApiTest() {
         Allure.step {
-            sleep(5_000L)
-            println("#2 API Test")
+            sleep(1_000L)
+            --counter
+            if (counter == 0) {
+                println("#2 API Test")
+            } else {
+                --counter
+                Assert.fail()
+            }
         }
     }
 }
